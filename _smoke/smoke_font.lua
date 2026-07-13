4c78a16a6fb36ddf18ee5a6e537e753435c1e172
Brice Lancaster <bricelancaster.wcp@gmail.com>
2026-07-13 15:12:11 -0500

    test: smoke configs for threading, font, system, exceptions, pngdec, audio samples
    All twelve samples now run clean without KYTY_PERMISSIVE: hello,
    graphics, input, SDL2, audio-wav, threading, font, system, keyboard,
    exceptions (c++ unwinding passes), dialogs, pngdec.

---
+++ _smoke/smoke_font.lua
@@ -0,0 +1,42 @@
+local cfg = {
+	ScreenWidth = 1280;
+	ScreenHeight = 720;
+	Neo = true;
+	VulkanValidationEnabled = false;
+	ShaderValidationEnabled = false;
+	ShaderOptimizationType = 'Performance';
+	ShaderLogDirection = 'Silent';
+	ShaderLogFolder = '_Shaders';
+	CommandBufferDumpEnabled = false;
+	CommandBufferDumpFolder = '_Buffers';
+	PrintfDirection = 'Console';
+	PrintfOutputFile = '_kyty.txt';
+	ProfilerDirection = 'None';
+	ProfilerOutputFile = '_profile.prof';
+}
+
+kyty_init(cfg);
+
+kyty_mount('/home/brice/Projects/kyty-assets/openorbis/OpenOrbis/PS4Toolchain/samples/font', '/app0');
+
+kyty_load_elf('/app0/font/x64/Debug/font.oelf');
+kyty_load_elf('/app0/sce_module/libc.prx', 0);
+kyty_load_elf('/app0/sce_module/libSceFios2.prx', 0);
+
+kyty_load_symbols('libAudio_1');
+kyty_load_symbols('libc_internal_1');
+kyty_load_symbols('libDebug_1');
+kyty_load_symbols('libDialog_1');
+kyty_load_symbols('libDiscMap_1');
+kyty_load_symbols('libFreeType_1');
+kyty_load_symbols('libGraphicsDriver_1');
+kyty_load_symbols('libkernel_1');
+kyty_load_symbols('libNet_1');
+kyty_load_symbols('libPad_1');
+kyty_load_symbols('libPlayGo_1');
+kyty_load_symbols('libSysmodule_1');
+kyty_load_symbols('libSystemService_1');
+kyty_load_symbols('libUserService_1');
+kyty_load_symbols('libVideoOut_1');
+
+kyty_execute();
