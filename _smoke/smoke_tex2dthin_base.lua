4bd9ed90b2063c725f77ecf9423190b974efebd2
Brice Lancaster <bricelancaster.wcp@gmail.com>
2026-07-15 22:22:29 -0500

    test: add base-mode (Neo=false) smoke config for tex2dthin

---
+++ _smoke/smoke_tex2dthin_base.lua
@@ -0,0 +1,17 @@
+local cfg = {
+	ScreenWidth = 1920; ScreenHeight = 1080; Neo = false;
+	VulkanValidationEnabled = false; ShaderValidationEnabled = false;
+	ShaderOptimizationType = 'Performance'; ShaderLogDirection = 'Console';
+	ShaderLogFolder = '_Shaders'; CommandBufferDumpEnabled = false;
+	CommandBufferDumpFolder = '_Buffers'; PrintfDirection = 'Console';
+	PrintfOutputFile = '_kyty.txt'; ProfilerDirection = 'None';
+	ProfilerOutputFile = '_profile.prof';
+}
+kyty_init(cfg);
+kyty_mount('/home/brice/Projects/kyty-assets/freegnm-examples/tex2dthin', '/app0');
+kyty_load_elf('/app0/tex2dthin_base.oelf');
+kyty_load_symbols('libc_internal_1');
+kyty_load_symbols('libkernel_1');
+kyty_load_symbols('libGraphicsDriver_1');
+kyty_load_symbols('libVideoOut_1');
+kyty_execute();
